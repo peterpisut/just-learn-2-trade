e1901486ce698c1322e4184d02a543a08c13a390
altrr <peterpisut@gmail.com>
2021-08-07 01:41:31 +0700

    Add topbar and footer

--- front-justLearn2Trade/src/app/app.module.ts
+++ front-justLearn2Trade/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './share/topbar/topbar.component';
+import { FooterComponent } from './share/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
